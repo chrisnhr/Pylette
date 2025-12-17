aa8d931909c16563e20c49a5ec765bd87b78f851
chrisnhr <christoffer.naeher@gmail.com>
2025-12-17 11:45:21 +0100

    fix: add copy parameter to HDBSCAN model for better handling of input data

--- Pylette/src/extractors/hdbscan.py
+++ Pylette/src/extractors/hdbscan.py
@@ -25,7 +25,7 @@ class HDBSCANExtractor(ColorExtractorBase):
         from sklearn.cluster import HDBSCAN
 
         arr = np.squeeze(arr)
-        model = HDBSCAN(min_cluster_size=int(len(arr) / 10), allow_single_cluster=True) # no cluster with less than 10% of points
+        model = HDBSCAN(min_cluster_size=int(len(arr) / 10), allow_single_cluster=True, copy = True) # no cluster with less than 10% of points
         labels = model.fit_predict(arr)
         unique_labels, counts = np.unique(labels, return_counts=True)
         label_freq = counts / counts.sum()
