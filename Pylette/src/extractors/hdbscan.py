fix: adjust min_cluster_size calculation in HDBSCANExtractor for better clustering

Pylette/src/extractors/hdbscan.py
@@ -25,7 +25,7 @@ class HDBSCANExtractor(ColorExtractorBase):
         from sklearn.cluster import HDBSCAN
 
         arr = np.squeeze(arr)
-        clusterer = HDBSCAN(min_cluster_size=max(2, len(arr) // (palette_size * 10)), prediction_data=True)
+        clusterer = HDBSCAN(min_cluster_size=len(arr) / 10, allow_single_cluster=True) # no cluster with less than 10% of points
         labels = clusterer.fit_predict(arr)
         unique_labels, counts = np.unique(labels, return_counts=True)
         label_freq = counts / counts.sum()